56d4662f3a159099e33642d35fd459f1c13686f3
Kamil Zielinski <k.zielinski@text.com>
2024-04-18 18:42:52 +0200

    format

--- internal/model/incoming_event.go
+++ internal/model/incoming_event.go
@@ -1,14 +1,14 @@
 package model
 
 type IncomingEventPayload struct {
-	ChatId string `json:"chat_id"`
+	ChatId   string `json:"chat_id"`
 	ThreadId string `json:"thread_id"`
 }
 
 type IncomingEvent struct {
-	WebhookId string `json:"webhook_id"`
-	SecretKey string `json:"secret_key"` // how to verify it?
-	Action string `json:"action"`
-	OrganizationId string `json:"organization_id"`
-	Payload IncomingEventPayload `json:"payload"`
+	WebhookId      string               `json:"webhook_id"`
+	SecretKey      string               `json:"secret_key"` // how to verify it?
+	Action         string               `json:"action"`
+	OrganizationId string               `json:"organization_id"`
+	Payload        IncomingEventPayload `json:"payload"`
 }
